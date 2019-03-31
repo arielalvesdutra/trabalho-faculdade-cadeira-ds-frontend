fafb845995d8bbed6169c0338e5e490407f9f33f
arielalvesdutra <alves.lsn@gmail.com>
2019-03-31 17:39:23 -0300

    Adiciona carregamento das justificativas nos arquivos assets/js/consultar-horas.js e assets/js/editar-horas.js

--- assets/js/consultar-horas.js
+++ assets/js/consultar-horas.js
@@ -1,10 +1,47 @@
-
 import { getSelectedProfile, getUserPayload, validadeProfileSelection, validateToken } from './auth.js'
 import HourAdjustmentService from './hour-adjustment-service.js'
+import JustificationService from './justification-service.js'
 
-const startUp = () => {
+const buildJustificationOptions = (justifications) => {
+    let justificationOptions = '<option value="0">Favor selecionar a justificativa...</option>'
+
+    let justificationsArray = Object.values(justifications)
+
+    justificationsArray.forEach(justifications => {
+
+        justificationOptions += `<option value="${justifications['id']}">${justifications['title']}</option >`
+
+    });
+
+    return justificationOptions
+}
+
+const fillJustificationOptions = (justifications) => {
+    let justificationOptions = document.getElementById('justification-options')
+    let optionsElements = buildJustificationOptions(justifications)
+
+    justificationOptions.innerHTML = optionsElements
+}
+
+const getJustifications = async () => {
+
+    let justificationService = new JustificationService()
+
+    let justifications = await justificationService.getJustifications()
+
+    return justifications
+}
+
+const startUp = async () => {
     validateToken()
     validadeProfileSelection()
+
+    try {
+        let justifications = await getJustifications()
+        fillJustificationOptions(justifications)
+    } catch(error) {
+
+    }
 }
 
 startUp()
