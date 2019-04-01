e49c903d9c529cf4c01184706f400118e7944caf
arielalvesdutra <alves.lsn@gmail.com>
2019-03-31 21:12:31 -0300

    Adiciona verificação para o status 401 no handleErrors() no  assets/js/handle-errors.js

--- assets/js/handle-errors.js
+++ assets/js/handle-errors.js
@@ -1,5 +1,17 @@
+import { isLogged, logOut } from './auth.js'
+
 const handleErrors = (response) => {
     if (!response.ok) {
+
+        if (response.status === 401) {
+
+            if (isLogged) {
+                logOut('Sessão expirou. Favor fazer login novamente!')
+            } else {
+                logOut('Usuário não logado!')
+            }
+        }
+
         throw response
     }
     return response;
