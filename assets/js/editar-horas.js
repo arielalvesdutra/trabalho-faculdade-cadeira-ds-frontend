e67beda53893aa6575bc90563ab9fe5eba390017
arielalvesdutra <alves.lsn@gmail.com>
2019-04-03 11:15:29 -0300

    Adiciona a listagem de ajustes de horas nas páginas consultar-horas.html e editar-horas.html

--- assets/js/editar-horas.js
+++ assets/js/editar-horas.js
@@ -1,7 +1,46 @@
-
 import { getSelectedProfile, getUserPayload, validadeProfileSelection, validateToken } from './auth.js'
 import HourAdjustmentService from './hour-adjustment-service.js'
 import JustificationService from './justification-service.js'
+
+const buildAdjustmentsListTbody = (hoursAdjustments, justifications) => {
+
+    let tbody = ''
+    
+    if (hoursAdjustments) {
+        let justificationsArray = formatJusticationsIndexWithId(justifications)
+        let hoursAdjustmentsArray = Object.values(hoursAdjustments)
+
+        hoursAdjustmentsArray.forEach((adjustment) => {
+            tbody +=
+                `<tr id="tr_${adjustment['id']}">
+                <td id="td_date_${adjustment['id']}">${adjustment["date"]}</td>
+                <td id="td_entry_${adjustment['id']}">${adjustment["entryHour"]}</td>
+                <td id="td_exit_${adjustment['id']}">${adjustment["exitHour"]}</td>
+                <td id="td_duration_${adjustment['id']}">${adjustment["duration"]}</td>
+                <td id="td_justification_${adjustment['id']}">${justificationsArray['id_' + adjustment["justification"]["id"]]}</td>
+                <td id="td_actions_${adjustment['id']}">
+                    <button id="editButton" value="${adjustment['id']}" 
+                            class="edit-button" title="Editar ajuste">
+                        <i class="far fa fa-edit"></i>
+                    </button>
+                    <button id="deleteButton" value="${adjustment['id']}" 
+                            class="delete-button" title="Excluir ajuste">
+                        <i class="far fa fa-trash"></i>
+                    </button>    
+                </td>
+            </tr>`
+        })
+    } else {
+        tbody =
+            `<tr>
+                <td colspan="6">
+                    <b>Nenhum ajuste encontrado.</b>
+                </td>
+            </tr>`
+    }
+
+    return tbody
+}
 
 const buildJustificationOptions = (justifications) => {
     let justificationOptions = '<option value="0">Favor selecionar a justificativa...</option>'
@@ -17,11 +56,53 @@ const buildJustificationOptions = (justifications) => {
     return justificationOptions
 }
 
+const fillAdjustmentsListTable = (hoursAdjustments, justifications) => {
+
+    let tbody
+    let adjustmentsListTable = document.querySelector('#adjustments-list-table')
+    let adjustmentsListTbody = adjustmentsListTable.querySelector('tbody')
+
+    if (hoursAdjustments){
+        tbody = buildAdjustmentsListTbody(hoursAdjustments, justifications)
+    } else {
+        tbody = buildAdjustmentsListTbody(null, null)
+    }
+
+    adjustmentsListTbody.innerHTML = tbody
+}
+
 const fillJustificationOptions = (justifications) => {
     let justificationOptions = document.getElementById('justification-options')
     let optionsElements = buildJustificationOptions(justifications)
 
     justificationOptions.innerHTML = optionsElements
+}
+
+const formatJusticationsIndexWithId = justifications => {
+
+    let justificationsArray = []
+
+    if (justifications) {
+        for (let justification of justifications) {
+            justificationsArray[`id_${justification.id}`] = justification.title
+        }
+    }
+
+    return justificationsArray
+}
+
+const getHoursAdjustments = async () => {
+    try {
+
+        let hourAdjustmentService = new HourAdjustmentService()
+
+        let hoursAdjustments = hourAdjustmentService.getEmployeeAdjustments()
+
+        return hoursAdjustments
+
+    } catch (error) {
+
+    }
 }
 
 const getJustifications = async () => {
@@ -33,15 +114,34 @@ const getJustifications = async () => {
     return justifications
 }
 
+const setProfileTitle = profileTitle => {
+    const profileTitleDiv = document.getElementById('profile-title')
+    profileTitleDiv.innerHTML = "Perfil: " + profileTitle
+}
+
+const setUserNameTitle = userName => {
+    const profileTitleDiv = document.getElementById('user-name-title')
+    profileTitleDiv.innerHTML = "Usuário: " + userName
+}
+
 const startUp = async () => {
     validateToken()
     validadeProfileSelection()
 
+    let userPayload = getUserPayload()
+    setUserNameTitle(userPayload.name)
+
+    let selectedProfile = getSelectedProfile()
+    setProfileTitle(selectedProfile.name)
+
     try {
         let justifications = await getJustifications()
         fillJustificationOptions(justifications)
-    } catch(error) {
 
+        let hoursAdjustments = await getHoursAdjustments()
+        fillAdjustmentsListTable(hoursAdjustments, justifications)
+    } catch(error) {
+        fillAdjustmentsListTable(null, null)
     }
 }
 
